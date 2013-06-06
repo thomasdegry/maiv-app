46f4ee117511c224fb4062c2e516532582f7c465
Thomas Degry <degry.thomas@gmail.com>
2013-06-07 00:11:55 +0200

    Initial Commit

---
+++ mrburger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mrburger
+//
+//  Created by Thomas Degry on 07/06/13.
+//  Copyright (c) 2013 devine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
